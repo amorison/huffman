700aad7a4bb2029b9286dc7ac0284c5e7be2afec
Adrien Morison <adrien.morison@gmail.com>
2016-08-23 16:37:35 +0200

    Add computation of entropy
    Both optimal and obtained number of
    bits per symbol are now computed
    by the entropy function.

---
+++ Huffman/CodeBook/Statistics.hs
@@ -0,0 +1,14 @@
+module Huffman.CodeBook.Statistics
+( entropy
+) where
+
+import Huffman.CodeBook.Internal
+
+lg2 :: (Floating a) => a -> a
+lg2 = logBase 2
+
+entropy :: (Floating a) => CodeBook s -> (a, a)
+entropy = (\(eO, eH, wTot) -> (lg2 wTot - eO/wTot, eH/wTot)) . foldr fn (0,0,0)
+    where fn (_,w,l,_) (eO, eH, wTot) = (eO+wi*lg2 wi, eH+li*wi, wTot+wi)
+            where wi = fromIntegral w
+                  li = fromIntegral l
--- Main.hs
+++ Main.hs
@@ -1,5 +1,16 @@
 module Main(main) where
 
-import Huffman.CodeBook.Builder(huffmanEncode)
+import Huffman.CodeBook.Builder(CodeBook, huffmanEncode)
+import Huffman.CodeBook.Statistics(entropy)
 
-main = interact $ unlines . map show . huffmanEncode
+printCodeBook :: (Ord s, Show s) => CodeBook s -> String
+printCodeBook = unlines . map show
+
+main = do
+    message <- getContents
+    let codeBook = huffmanEncode message
+        (opt, enc) = entropy codeBook
+    putStr $ printCodeBook codeBook
+    putStrLn $ "\nInformation content (bits/symbol):"
+    putStrLn $ "Entropy: " ++ show opt
+    putStrLn $ "Encoding: " ++ show enc
