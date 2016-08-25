7837879062bd8efeabb3c6f858401fac56a0830f
Adrien Morison <adrien.morison@gmail.com>
2016-08-25 15:34:48 +0200

    Add some Statistics functions
    These will be useful to determine the best
    way to encode the tree.

--- Huffman/Statistics.hs
+++ Huffman/Statistics.hs
@@ -1,5 +1,7 @@
 module Huffman.Statistics
 ( entropy
+, nBitsCodeLength
+, totalEncodedLength
 ) where
 
 import Huffman.Internal
@@ -7,8 +9,37 @@ import Huffman.Internal
 lg2 :: (Floating a) => a -> a
 lg2 = logBase 2
 
+-- compute (optimal bits/symbol, obtained bits/symbol)
+--   the obtained b/s is computed without taking the cost of
+--   encoding the tree into account
 entropy :: (Floating a) => CodeBook -> (a, a)
-entropy = (\(eO, eH, wTot) -> (lg2 wTot - eO/wTot, eH/wTot)) . foldr fn (0,0,0)
-    where fn (_,w,l,_) (eO, eH, wTot) = (eO+wi*lg2 wi, eH+li*wi, wTot+wi)
-            where wi = fromIntegral w
-                  li = fromIntegral l
+entropy cb = (entropyShannon cb, lTot/wTot)
+    where lTot = fromIntegral $ totalEncodedLength cb
+          wTot = fromIntegral $ totalMsgLength cb
+
+-- compute Shannon entropy
+entropyShannon :: (Floating a) => CodeBook -> a
+entropyShannon cb = sum $ map ((\p -> (-p)*lg2 p) . getP) cb
+    where getP = (/wTot) . fromIntegral . getSymbolWeight
+          wTot = fromIntegral $ totalMsgLength cb
+
+-- extract weight of a CodeBook entry
+getSymbolWeight :: (Symbol, Weight, CodeLength, HuffmanCode) -> Weight
+getSymbolWeight (_, w, _, _) = w
+
+-- extract code length of a CodeBook entry
+getCodeLength :: (Symbol, Weight, CodeLength, HuffmanCode) -> CodeLength
+getCodeLength (_, _, l, _) = l
+
+-- necessary number of bits to encode CodeLength
+nBitsCodeLength :: CodeBook -> Int
+nBitsCodeLength = ceiling . lg2 . fromIntegral . maximum . map getCodeLength
+
+-- total length in bytes of initial message
+totalMsgLength :: CodeBook -> Weight
+totalMsgLength = sum . map getSymbolWeight
+
+-- total length in bits of encoded message
+totalEncodedLength :: CodeBook -> CodeLength
+totalEncodedLength = foldr go 0
+    where go (_, w, l, _) lTot = lTot + w*l
